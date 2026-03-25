bfa7effaf69678d6dc2cf7c0d384491eebf11774
sumiran-nagture <sumiran.nagture@hitachienergy.com>
2026-03-25 08:01:00 +0000

    Add files via upload
    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Welcome " + name + "!");
+
+        Console.ReadLine(); 
+    }
+}
